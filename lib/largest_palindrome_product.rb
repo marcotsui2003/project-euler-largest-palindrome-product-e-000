8989423203d4169d0716e6dc88d05df1685a2c4d
marcotsui2003 <marcotsui2003@yahoo.com>
2015-10-28 20:35:22 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,23 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  result = 1
+  for i in (1..999)
+    for j in (1..999)
+      if palindrome?(i*j) && (i*j)  > result
+          result = i*j
+      end
+    end
+  end
+  result
+end
+
+def palindrome?(num)
+  if num == num.to_s.reverse.to_i
+    true
+  else
+    false
+  end
+end
+
+
+
+
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,15 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  
+
+  def answer
+    result = 1
+    for i in (1..999)
+      for j in (1..999)
+        if i*j == (i*j).to_s.reverse.to_i  && (i*j)  > result
+          result = i*j
+        end
+      end
+    end
+  result
+  end
+end
